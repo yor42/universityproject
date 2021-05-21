f690cad680a81452cac1449e689e9911b2bc087c
yor42 <yoonch1022@naver.com>
2021-05-21 17:44:19 +0900

    data generator+dataset fix

--- Main.py
+++ Main.py
@@ -36,60 +36,48 @@ test_negative_dir = os.path.join(test_dir, 'negative')
 Path(test_positive_dir).mkdir(parents=True, exist_ok=True)
 Path(test_negative_dir).mkdir(parents=True, exist_ok=True)
 
-fnames = [f'{i}.jpg' for i in range(0, 5000)]
+fnames = os.listdir(dataset_positive)
 
-if len(os.listdir(validation_positive_dir)) < 5000:
-    print("양성 검증 데이터셋 개수가 예상보다 적습니다! 생성중...")
-    for f in fnames:
-        src = os.path.join(dataset_positive, f)
-        dst = os.path.join(validation_positive_dir, f)
-        shutil.copyfile(src, dst)
+if len(os.listdir(validation_positive_dir)) < 1000:
+    for i, f in enumerate(fnames):
+        if i < 1000:
+            src = os.path.join(dataset_positive, f)
+            dst = os.path.join(validation_positive_dir, str(i) + ".jpg")
+            shutil.copyfile(src, dst)
+        elif i<2000:
+            src = os.path.join(dataset_positive, f)
+            dst = os.path.join(test_positive_dir, str(i) + ".jpg")
+            shutil.copyfile(src, dst)
+        else:
+            src = os.path.join(dataset_positive, f)
+            dst = os.path.join(train_positive_dir, str(i) + ".jpg")
+            shutil.copyfile(src, dst)
 
-fnames = [f'{i}.jpg' for i in range(5000, 10000)]
+fnames = os.listdir(dataset_negative)
 
-if len(os.listdir(validation_negative_dir)) < 5000:
-    print("음성 검증 데이터셋 개수가 예상보다 적습니다! 생성중...")
-    for f in fnames:
-        src = os.path.join(dataset_negative, f)
-        dst = os.path.join(validation_negative_dir, f)
-        shutil.copyfile(src, dst)
+if len(os.listdir(validation_negative_dir)) < 1000:
+    for i, f in enumerate(fnames):
+        if i < 1000:
+            src = os.path.join(dataset_negative, f)
+            dst = os.path.join(validation_negative_dir, str(i) + ".jpg")
+            shutil.copyfile(src, dst)
+        elif i<2000:
+            src = os.path.join(dataset_negative, f)
+            dst = os.path.join(test_positive_dir, str(i) + ".jpg")
+            shutil.copyfile(src, dst)
+        else:
+            src = os.path.join(dataset_negative, f)
+            dst = os.path.join(train_negative_dir, str(i) + ".jpg")
+            shutil.copyfile(src, dst)
 
-if len(os.listdir(test_positive_dir)) < 5000:
-    print("양성 시험 데이터셋 개수가 예상보다 적습니다! 생성중...")
-    for f in fnames:
-        src = os.path.join(dataset_positive, f)
-        dst = os.path.join(test_positive_dir, f)
-        shutil.copyfile(src, dst)
-
-fnames = [f'{i}.jpg' for i in range(5000, 10000)]
-
-if len(os.listdir(test_negative_dir)) < 5000:
-    print("음성 시험 데이터셋 개수가 예상보다 적습니다! 생성중...")
-    for f in fnames:
-        src = os.path.join(dataset_negative, f)
-        dst = os.path.join(test_negative_dir, f)
-        shutil.copyfile(src, dst)
-
-fnames = [f'{i}.jpg' for i in range(10000, len(os.listdir(dataset_positive))-1)]
-
-print("훈련 데이터 폴더를 생성중입니다. 기다리세요.")
-for f in fnames:
-    src = os.path.join(dataset_positive, f)
-    dst = os.path.join(train_positive_dir, f)
-    shutil.copyfile(src, dst)
-fnames = [f'{i}.jpg' for i in range(10000, len(os.listdir(dataset_negative))-1)]
-for f in fnames:
-    src = os.path.join(dataset_negative, f)
-    dst = os.path.join(train_negative_dir, f)
-    shutil.copyfile(src, dst)
 
 train_datagen = ImageDataGenerator()
 validation_datagen = ImageDataGenerator()
 
-train_generator = train_datagen.flow_from_directory(train_dir, batch_size=20, class_mode='binary')
-validation_generator = validation_datagen.flow_from_directory(validation_dir, batch_size=20, class_mode='binary')
+train_generator = train_datagen.flow_from_directory(train_dir, batch_size=20, class_mode='binary', target_size=(256,256))
+validation_generator = validation_datagen.flow_from_directory(validation_dir, batch_size=20, class_mode='binary', target_size=(256,256))
 
-base = InceptionResNetV2(weights='imagenet', include_top=False, input_shape=(227, 227, 3))
+base = InceptionResNetV2(weights='imagenet', include_top=False, input_shape=(256, 256, 3))
 
 model = Sequential()
 model.add(base)
@@ -101,4 +89,5 @@ model.add(Dense(1, activation='sigmoid', kernel_regularizer=regularizers.l2(0.00
 
 model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
 
-history = model.fit_generator(train_generator, steps_per_epoch=100, epochs=30, validation_data=validation_generator, validation_steps=50)
+history = model.fit_generator(train_generator, steps_per_epoch=100, epochs=30, validation_data=validation_generator,
+                              validation_steps=50)
