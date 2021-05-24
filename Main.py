65da0f1f1ac02b3b78e6325dd515aef9bc9e9510
yor42 <yoonch1022@naver.com>
2021-05-24 16:57:52 +0900

    added data argumentation

--- Main.py
+++ Main.py
@@ -1,19 +1,18 @@
-
 from keras import regularizers
 from tensorflow.python.keras.applications.inception_resnet_v2 import InceptionResNetV2
-from tensorflow.python.keras.layers import Dropout, Flatten, Dense
+from tensorflow.python.keras.layers import Dropout, Flatten, Dense, GlobalAveragePooling2D
 from tensorflow.python.keras.models import Sequential
 from keras.preprocessing.image import ImageDataGenerator
 from pathlib import Path
 import matplotlib.pyplot as plt
 import random
 import os, shutil
-
+import efficientnet.keras as efn
 import tensorflow as tf
+
 config = tf.compat.v1.ConfigProto()
 config.gpu_options.allow_growth = True
 session = tf.compat.v1.Session(config=config)
-
 
 # 폴더가 없으면 폴더를 만듬
 dataset_dir = './dataset'
@@ -54,7 +53,7 @@ if len(os.listdir(validation_positive_dir)) < 1000:
             src = os.path.join(dataset_positive, f)
             dst = os.path.join(validation_positive_dir, str(i) + ".jpg")
             shutil.copyfile(src, dst)
-        elif i<2000:
+        elif i < 2000:
             src = os.path.join(dataset_positive, f)
             dst = os.path.join(test_positive_dir, str(i) + ".jpg")
             shutil.copyfile(src, dst)
@@ -72,7 +71,7 @@ if len(os.listdir(validation_negative_dir)) < 1000:
             src = os.path.join(dataset_negative, f)
             dst = os.path.join(validation_negative_dir, str(i) + ".jpg")
             shutil.copyfile(src, dst)
-        elif i<2000:
+        elif i < 2000:
             src = os.path.join(dataset_negative, f)
             dst = os.path.join(test_positive_dir, str(i) + ".jpg")
             shutil.copyfile(src, dst)
@@ -81,21 +80,32 @@ if len(os.listdir(validation_negative_dir)) < 1000:
             dst = os.path.join(train_negative_dir, str(i) + ".jpg")
             shutil.copyfile(src, dst)
 
-
-train_datagen = ImageDataGenerator()
+train_datagen = ImageDataGenerator(
+    rotation_range=40,
+    width_shift_range=0.3,
+    height_shift_range=0.3,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    vertical_flip=True
+)
 validation_datagen = ImageDataGenerator()
 
-train_generator = train_datagen.flow_from_directory(train_dir, batch_size=20, class_mode='binary', target_size=(256,256))
-validation_generator = validation_datagen.flow_from_directory(validation_dir, batch_size=20, class_mode='binary', target_size=(256,256))
+train_generator = train_datagen.flow_from_directory(train_dir, batch_size=20, class_mode='binary',
+                                                    target_size=(256, 256))
+validation_generator = validation_datagen.flow_from_directory(validation_dir, batch_size=20, class_mode='binary',
+                                                              target_size=(256, 256))
 
-net_base = InceptionResNetV2(weights='imagenet', include_top=False, input_shape=(256, 256, 3))
+# net_base = InceptionResNetV2(weights='imagenet', include_top=False, input_shape=(256, 256, 3))
+net_base = InceptionResNetV2(weights='imagenet', include_top=False)
 
-net_base.trainable = True
-set_trainable = False
 flag = False
-if(flag):
+if (flag):
+
+    net_base.trainable = True
+    set_trainable = False
     for layer in net_base.layers:
-        if layer.name== "block8_10_conv":
+        if layer.name == "block8_10_conv":
             set_trainable = True
         if set_trainable:
             layer.trainable = True
@@ -104,14 +114,14 @@ if(flag):
 
 model = Sequential()
 model.add(net_base)
-model.add(Flatten())
+model.add(GlobalAveragePooling2D())
 model.add(Dense(256, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
 model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
 
-history = model.fit(train_generator, steps_per_epoch=100, epochs=100, validation_data=validation_generator,
-                              validation_steps=50)
+history = model.fit(train_generator, steps_per_epoch=50, epochs=100, validation_data=validation_generator,
+                    validation_steps=50)
 
 history_dict = history.history
 accuracy = history_dict['accuracy']
